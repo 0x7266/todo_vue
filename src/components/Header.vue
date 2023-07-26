29fd3cf96d8c223cc64d46f4db9f90fa98cd3ea0
unknown <rrrr0ffff@gmail.com>
2023-07-26 01:30:49 -0300

    basic header styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,11 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-	<strong>📃</strong>
-	<nav>
-		<RouterLink to="/">Home</RouterLink>
-		<RouterLink to="/about">About</RouterLink>
-	</nav>
+	<header class="bg-zinc-300 w-full flex justify-around items-center pt-3 pb-5">
+		<strong class="text-5xl">📃</strong>
+		<nav class="flex gap-4 text-2xl font-semibold">
+			<RouterLink to="/">home</RouterLink>
+			<RouterLink to="/about">about</RouterLink>
+		</nav>
+	</header>
 </template>
