c187dce68c680447f3fea5c8c16c7100c931acc6
unknown <rrrr0ffff@gmail.com>
2023-07-26 01:22:26 -0300

    added basic navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+	<strong>📃</strong>
+	<nav>
+		<RouterLink to="/">Home</RouterLink>
+		<RouterLink to="/about">About</RouterLink>
+	</nav>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TodoList from "../components/TodoList.vue";
+</script>
 
 <template>
-	<h1>Home</h1>
+	<TodoList />
 </template>
